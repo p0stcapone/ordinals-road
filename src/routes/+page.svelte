65b24721aaf911da1561f90ff86f95b91e27c56c
Post Capone <p0stc4p0n3@gmail.com>
2023-05-25 14:43:43 -0400

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,56 @@
+<section class="main">
+    <img class="pepe-logo" src="/ordRoadPepe.png" alt="logo" />
+    <img class="logo-text" src="ordRoadLogoText.png" alt="logo text" />
+</section>
+<nav>
+    <h1>...Coming Soon</h1>
+</nav>
+<section class="socials">
+    <a href="https://twitter.com/OrdinalsRoad" target="_blank">
+        <img src="twitterLogo.png" alt="twitter" />
+    </a>
+</section>
+
+<style>
+    .main {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    nav {
+        position: absolute;
+        top: 25px;
+        left: 25px;
+    }
+
+    nav h1 {
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 2.5rem;
+    }
+
+    .socials {
+        position: absolute;
+        top: 25px;
+        right: 25px;
+        width: 70px;
+        height: 70px;
+        display: flex;
+    }
+
+    .socials * {
+        width: 100%;
+        height: 100%;
+    }
+
+    .pepe-logo {
+        height: 50vh;
+    }
+
+    .logo-text {
+        width: 50%;
+    }
+</style>
